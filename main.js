1874d28cbdd94afbd63f0e9ecca898b798ada391
Bach-sys <bachloppho1202@gmail.com>
2023-02-19 20:10:07 +0700

    change

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 fetch("https://theblackwomanhistory.firebaseio.com/.json")
 .then(response => response.json())
 .then(post => console.log(post))
-=======
-function hello() {
-    setTimeout((e) => {
-        e = "hi"
-        return e
-    }, 2000);
-}
-
-function hi(){
-    console,log("hello")
-    const result = hello()
-    console.log(result)
-}
->>>>>>> f8b8f5293949d396d71bc2d74fb4aa4fab6df6c0
+// =======
+//>>>>>>> f8b8f5293949d396d71bc2d74fb4aa4fab6df6c0
